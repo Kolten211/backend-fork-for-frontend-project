bbec35fc64567325d9bcc1d373b4b582b8416567
Kolten <kolten.smith1@gmail.com>
2024-10-11 14:15:30 -0700

    phase 3 complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,53 @@
+// frontend/src/App.jsx
+
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import Navigation from './components/Navigation/Navigation';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: "login",
+        element: <LoginFormPage />
+      },
+      {
+        path: "signup",
+        element: <SignupFormPage />
+      }
+    ]
+  }
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
