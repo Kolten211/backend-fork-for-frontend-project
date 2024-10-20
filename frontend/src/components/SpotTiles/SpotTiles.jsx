673ec2a74e8db334d104989a283fc62cb4b10853
Kolten <kolten.smith1@gmail.com>
2024-10-20 13:04:43 -0700

    we got this

---
+++ frontend/src/components/SpotTiles/SpotTiles.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaStar } from "react-icons/fa";
+import './SpotTiles.css'
+
+function SpotTile({ spot }) {
+    return (
+      <NavLink to={`/spots/${spot.id}`} className="spot-tile-link">
+        <div className="spot-tile">
+          <img src={spot.previewImage || 'default-image-url'} alt={spot.name} className="spot-image" />
+          <h3>{spot.name}</h3>
+          <div className='spot-info'>
+            <p>{spot.city}, {spot.state}</p>
+            <p className='spot-rating'><FaStar />{spot.avgStarRating}</p>
+          </div>
+          <p>${spot.price}night</p>
+        </div>
+      </NavLink>
+    );
+  }
+
+function SpotTiles() {
+    const [spotList, setSpotList] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/spots')
+         .then(response => response.json())
+         .then(data =>  {
+            console.log('Fetched spots:', data.Spots);
+            setSpotList(data.Spots);
+          })
+         .catch(error => console.error('Error Fetching Spots', error))
+    }, []);
+
+    return (
+        <div className='spot-tiles-container'>
+            {spotList.map(spot => (
+                <SpotTile key={spot.id} spot={spot} />
+            ))}
+        </div>
+    )
+}
+export default SpotTiles
